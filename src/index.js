0e122fb3eda6068839c69c591d982c1b63a2a79f
_Jogui_ <josueaguilarjnai@gmail.com>
2021-07-24 20:44:07 -0500

    Almost done

--- src/index.js
+++ src/index.js
@@ -9,8 +9,13 @@ if (require('electron-squirrel-startup')) { // eslint-disable-line global-requir
 const createWindow = () => {
   // Create the browser window.
   const mainWindow = new BrowserWindow({
-    width: 800,
+    width: 1000,
     height: 600,
+    webPreferences: {
+      nodeIntegration: true,
+      contextIsolation: false,
+      enableRemoteModule: true,
+    },
   });
 
   // and load the index.html of the app.
